ac97ecebe85fbe39f456e67d7c81393c6bc4626f
anideaofhappiness <anideaofhappiness@users.noreply.github.com>
2019-11-13 13:12:52 -0500

    Update clickity.R

--- clickity.R
+++ clickity.R
@@ -8,15 +8,8 @@ setwd("~/Desktop/nanogenmo_2019")
 
 jekyll_hyde <- gutenberg_download(43)
 jekyll_hyde2 <- gutenberg_download(43)
-clickity_clackity = c("click", "ity", "clack", "ity")
 
-jekyll_hyde2[, 2] <- apply(jekyll_hyde2[, 2], 2, function(x) gsub("his", "click", x))
+# jekyll_hyde2[, 2] <- apply(jekyll_hyde2[, 2], 2, function(x) gsub("his", "click", x))
 jekyll_hyde2[, 2] <- apply(jekyll_hyde2[, 2], 2, function(x) gsub(".", "click", x))
 
-nchar(jekyll_hyde$text)
-nchar(jekyll_hyde2$text)
-
 write_lines(jekyll_hyde2$text, "test.txt")
-
-write
-
