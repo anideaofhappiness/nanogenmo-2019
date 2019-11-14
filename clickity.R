cfb612c49051b0a8fc562c0ac4f40e88f57ff366
anideaofhappiness <anideaofhappiness@users.noreply.github.com>
2019-11-13 21:03:28 -0500

    Update clickity.R

--- clickity.R
+++ clickity.R
@@ -1,15 +1,24 @@
-library(readr)
-library(stringr)
 library(gutenbergr)
 library(tidytext)
+library(dplyr)
+
 
 # https://cran.r-project.org/web/packages/gutenbergr/vignettes/intro.html
-setwd("~/Desktop/nanogenmo_2019")
+# setwd("~/Desktop/nanogenmo_2019")
 
 jekyll_hyde <- gutenberg_download(43)
 jekyll_hyde2 <- gutenberg_download(43)
 
-# jekyll_hyde2[, 2] <- apply(jekyll_hyde2[, 2], 2, function(x) gsub("his", "click", x))
+# [, 2] is indicated 2nd column, the 2 means this is being applied to a column
 jekyll_hyde2[, 2] <- apply(jekyll_hyde2[, 2], 2, function(x) gsub(".", "click", x))
 
 write_lines(jekyll_hyde2$text, "test.txt")
+
+# next steps
+jekyll_hyde3 <- jekyll_hyde %>% mutate(nchar = nchar(text))
+
+newlist <- c("click", "clickity", "clickity-clack", "clickity-clackity ")
+
+# next: need to divide nchar to find out how many clickity clackitys and modulo or whatever to know how to end the line
+
+
